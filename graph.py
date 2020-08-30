6cd7a06946e3569d41fac560f5f6d8f81c3003da
ameyagh <ameyapg2@illinois.edu>
2020-08-30 13:40:23 -0500

    mel_spectograms

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
-import librosa
 import numpy as np
+import librosa
+import librosa.display
+import os
 
 def get_mel_spectrogram(path):
     y, sr = librosa.load(path)
@@ -9,4 +11,20 @@ def get_mel_spectrogram(path):
 
     return mel_spect
 
+def get_mel_spectrogram_for_each_genre():
+    genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+    mel_spectograms = []
 
+    for genre in genres:
+        path = os.listdir(f'genres/{genre}')[0]
+        path = f'genres/{genre}/{path}'
+        mel_spect = get_mel_spectrogram(path=path)
+        mel_spectograms.append(mel_spect)
+
+    return mel_spectograms
+
+
+if __name__ == '__main__':
+    l = get_mel_spectrogram_for_each_genre()
+    for s in l:
+        librosa.display.specshow(s, y_axis='mel', fmax=8000, x_axis='time')
